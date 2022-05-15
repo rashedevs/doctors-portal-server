bf636ad1c1863727e6d9db9643924f92165bc7cb
rashedevs <rasheduap2015@gmail.com>
2022-05-15 11:32:31 +0600

    create get api for available slots

--- index.js
+++ index.js
@@ -23,13 +23,57 @@ async function run() {
     const serviceCollection = client
       .db("doctors-portal")
       .collection("services");
+    const bookingCollection = client
+      .db("doctors-portal")
+      .collection("bookings");
 
     app.get("/service", async (req, res) => {
       const query = {};
       const cursor = serviceCollection.find(query);
       const services = await cursor.toArray();
-      console.log(services);
       res.send(services);
+    });
+
+    //not the proper way, we will use aggregate lookup, pipeline, match, group
+    app.get("/available", async (req, res) => {
+      const date = req.query.date;
+      //step1: get all services
+      const services = await serviceCollection.find().toArray();
+      //step2- get the booking of that day, o/p:[{},{},{}]
+      const query = { date: date };
+      const bookings = await bookingCollection.find(query).toArray();
+      //step3- for each service
+      services.forEach((service) => {
+        //step4-find bookings for that service, o/p: [{},{},{}]
+        const serviceBookings = bookings.filter(
+          (book) => book.treatment === service.name
+        );
+        //step5: select slots for the service bookings: ['','','']
+        const bookedSlots = serviceBookings.map((book) => book.slot);
+        //step6: select those slota that are not in bookedSlots
+        const available = service.slots.filter(
+          (slot) => !bookedSlots.includes(slot)
+        );
+        //step7: set available slots
+        service.slots = available;
+      });
+
+      res.send(services);
+    });
+
+    app.post("/booking", async (req, res) => {
+      const booking = req.body;
+      const query = {
+        treatment: booking.treatment,
+        date: booking.date,
+        patient: booking.patient,
+      };
+      const exists = await bookingCollection.findOne(query);
+      if (exists) {
+        return res.send({ success: false, booking: exists });
+      }
+      const result = await bookingCollection.insertOne(booking);
+      return res.send({ success: true, result });
     });
   } finally {
     // await client.close();
