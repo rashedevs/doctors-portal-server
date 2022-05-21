e39b702bd1ff20b690b3826819d8b4d0b66d46aa
rashedevs <rasheduap2015@gmail.com>
2022-05-21 18:25:00 +0600

    stripe payment intent end point created

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -55,6 +56,20 @@ async function run() {
         res.status(403).send({ message: "forbidden" });
       }
     };
+
+    //stripe intent api
+
+    app.post("/create-payment-intent", verifyJWT, async (req, res) => {
+      const service = req.body;
+      const price = service.price;
+      const amount = price * 100;
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ["card"],
+      });
+      res.send({ clientSecret: paymentIntent.client_secret });
+    });
 
     app.get("/service", async (req, res) => {
       const query = {};
